f064d49aaf8af4324f539057f3b49a68917d184b
Luke Chen <luke.chen.2010@gmail.com>
2020-07-19 21:53:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.addEventListener("keydown", function(e)
   {
     moveDodgerLeft();
   }
+  else if ()
 });
 
 function moveDodgerLeft() 
@@ -17,7 +18,18 @@ function moveDodgerLeft()
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
  
-  if (left > 0) {
+  if (left > 0) 
+  {
     dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() 
+{
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) 
+  {
+    dodger.style.left = `${left + 1}px`
   }
 }
