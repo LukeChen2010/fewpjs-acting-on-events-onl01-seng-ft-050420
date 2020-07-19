62f86e4439849a216423e8a8d2a982d11c15b986
Luke Chen <luke.chen.2010@gmail.com>
2020-07-19 21:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 let dodger = document.getElementById("dodger");
 
-dodger.style.backgroundColor = "#000000";
-
 dodger.style.backgroundColor = "#FF69B4";
 
+document.addEventListener("keydown", function(e) 
+{
+  if (e.key === "ArrowLeft") 
+  {
+    moveDodgerLeft();
+  }
+});
 
 function moveDodgerLeft() 
 {
